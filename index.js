96816679c3b3f5b29429862d5e6677218c80fc84
shubhartika0414 <pawarshubham81668@gmail.com>
2024-07-01 19:17:03 +0530

    space

--- index.js
+++ index.js
@@ -194,6 +194,12 @@ app.delete("/plant/:id", (req, res)=>{
     })
 })
 
+app.use("*", (req, res)=>{
+    res.send(`<div> 
+        <h1 style="text-align: center;"> 404 Not Found </h1>
+        </div>`)
+})
+
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
